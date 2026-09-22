c1761836859de2ffc834b248cd5865e4ea440894
Dolmong78 <ikojme@gmail.com>
2026-09-22 18:32:10 +0900

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ const applyMessage = document.getElementById("applyMessage");
 // 나중에 Google Apps Script를 만들고
 // 여기의 주소를 실제 주소로 바꿔주세요.
 
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxzcFJpdytIY6GkldR76jcMqUwB7fksicectXkWUpaZcHCPnv1FsfesvD6PCTWkM7i3fw/exec
-";
+const GOOGLE_SCRIPT_URL = "";
 
 
 // ==============================
